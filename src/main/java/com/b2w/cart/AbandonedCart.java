4b235079d2b4cad91682c3372345a542826e48eb
Wellington Azevedo <wellington.azevedo@bimprove.com>
2019-07-16 12:09:37 -0300

    Adicionando expose para serializar json de resposta, refatorando codigo

--- src/main/java/com/b2w/cart/AbandonedCart.java
+++ src/main/java/com/b2w/cart/AbandonedCart.java
@@ -5,8 +5,6 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import javax.xml.crypto.dsig.keyinfo.PGPData;
-
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
@@ -25,18 +23,23 @@ import com.google.gson.GsonBuilder;
 
 public class AbandonedCart {
 	
+	private final static String DATA_FORMAT = "yyyy-MM-dd HH:mm:ss";
+	private final static String PAGE_BASKET = "basket";
+	private final static String INPUT_PATH = "input/page-views.json";
+	private final static String OUTPUT_PATH = "output/abandoned-carts.json";
+	
 	public static void main(String[] args) {
 	
 		 PipelineOptions options = PipelineOptionsFactory.create();
 		 Pipeline p = Pipeline.create(options);
-		 PCollection<String> input = p.apply(TextIO.read().from("input/page-views.json"));
+		 PCollection<String> input = p.apply(TextIO.read().from(INPUT_PATH));
 		 
-		 PCollection<KV<String, PageViews>> jsonParseToPageViews = input.apply("teste", MapElements.via(
+		 PCollection<KV<String, PageViews>> jsonParseToPageViewsKV = input.apply("jsonParseToPageViewsKV", MapElements.via(
 				 new SimpleFunction<String, KV<String, PageViews>>() {
 					 @Override
 					 public KV<String, PageViews> apply(String input) {
 						 //System.out.println(input);
-						 GsonBuilder builder = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss");
+						 GsonBuilder builder = new GsonBuilder().setDateFormat(DATA_FORMAT);
 						 Gson gson = builder.create();
 						 PageViews pageViews = gson.fromJson(input, PageViews.class);
 						 String key = pageViews.getCustomer();
@@ -45,17 +48,19 @@ public class AbandonedCart {
 				 }));
 		 
 		 PCollection<KV<String, Iterable<PageViews>>> kvpCollection =
-				 jsonParseToPageViews.apply(GroupByKey.<String, PageViews>create());
+				 jsonParseToPageViewsKV.apply(GroupByKey.<String, PageViews>create());
 		 
 		 PCollection<String> resultado = kvpCollection.apply("Resultado", ParDo.of(new DoFn<KV<String, Iterable<PageViews>>, String>() {
+
+			 private Gson gson;
+			 
 			 @ProcessElement
 			 public void processElement(ProcessContext context) {
-				 Boolean isAbandoned = false;
 				 String key = context.element().getKey();
 				 Iterable<PageViews> pageViews = context.element().getValue();
 				 List<PageViews> listCustomerN = new ArrayList<>();
 				 for(PageViews page : pageViews) {
-					 System.out.println(page.toString());
+//					 System.out.println(page.toString());
 					 if(key.equals(page.getCustomer()))
 						 listCustomerN.add(page);
 				 }
@@ -64,32 +69,42 @@ public class AbandonedCart {
 							.collect(Collectors.toList());
 				 
 				 if(sortedList.size() > 1) {
-					 
+					 gson = new GsonBuilder().setDateFormat(DATA_FORMAT).excludeFieldsWithoutExposeAnnotation().create();
 					 for(int i = 1; i < sortedList.size(); i++) {
-					 	if(sortedList.get(i).getPage().equals("basket")) {
-					 		if( i >= 1 && (((sortedList.get(i).getTimestamp().getTime() - sortedList.get(i-1).getTimestamp().getTime())/1000)/60) >= 10 ) {
-					 			context.output(sortedList.get(i).toString());
+					 	if(sortedList.get(i).getPage().equals(PAGE_BASKET)) {
+					 		if( i >= 1 ) { 
+					 			Long tempoVisualizacaoAtual = sortedList.get(i).getTimestamp().getTime();
+					 			Long tempoVisualizacaoAnterior = sortedList.get(i-1).getTimestamp().getTime();
+					 			Long diferencaTempo = miliSegundosParaMinutos(tempoVisualizacaoAtual, tempoVisualizacaoAnterior);
+					 			if( diferencaTempo >= 10 ) {
+					 				context.output(gson.toJson(sortedList.get(i)));
+		 						}
 					 		} 
-					 		else if(i < sortedList.size() -1 && (((sortedList.get(i+1).getTimestamp().getTime() - sortedList.get(i).getTimestamp().getTime())/1000)/60) >= 10 ) {
-					 			context.output(sortedList.get(i+1).toString());
-					 		} else if (i+1 == sortedList.size()) {
-					 			context.output(sortedList.get(i).toString());
+					 		if(i < sortedList.size() -1) {
+					 			Long tempoVisualizacaoProximo = sortedList.get(i+1).getTimestamp().getTime();
+					 			Long tempoVisualizacaoAtual = sortedList.get(i).getTimestamp().getTime();
+					 			Long diferencaTempo = miliSegundosParaMinutos(tempoVisualizacaoProximo, tempoVisualizacaoAtual);
+					 			if( diferencaTempo >= 10 ) {
+					 				context.output(gson.toJson(sortedList.get(i)));
+	 							}	
+					 		} if (i+1 == sortedList.size()) {
+					 			context.output(gson.toJson(sortedList.get(i)));
 					 		}
 					 		
 					 	}
 					 }
 				 }
 				 
-//				 if(isAbandoned)
-//				 	context.output("");
-					 
-				 
 			 }
 			 
 		 }));
 		 
-		 resultado.apply(TextIO.write().to("output/abandoned-carts.json").withoutSharding());
+		 resultado.apply(TextIO.write().to(OUTPUT_PATH).withoutSharding());
 		 
 		 p.run().waitUntilFinish();
+	}
+	
+	public static Long miliSegundosParaMinutos(Long tempo, Long tempoAnterior) {
+		return ((tempo - tempoAnterior)/1000)/60;
 	}
 }
--- src/main/java/com/b2w/cart/model/PageViews.java
+++ src/main/java/com/b2w/cart/model/PageViews.java
@@ -3,15 +3,21 @@ package com.b2w.cart.model;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.google.gson.annotations.Expose;
+
 public class PageViews implements Serializable {
 	
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	@Expose
 	private Date timestamp;
+	@Expose
 	private String customer;
+	@Expose(serialize = false)
 	private String page;
+	@Expose
 	private String product;
 	
 	public Date getTimestamp() {
